f6aad0bfd0c81b1e939d14d2f52904e769211011
aruskin <asruskin@gmail.com>
2022-03-15 12:57:46 -0400

    Change copy_properties into more generic copy_entities to allow for copying of items

--- wikibase-manipulation.py
+++ wikibase-manipulation.py
@@ -61,21 +61,24 @@ def retrieve_credentials_and_get_token(session, config):
     csrf_token = csrf_token_response['query']['tokens']['csrftoken']
     return api_url, csrf_token, session
 
-def copy_properties(session, source_api_url, target_api_url, target_csrf_token, id_list, language_list, equiv_property=None):
-    properties_in = get_entities(source_api_url, id_list, language_list)
-    properties_in = properties_in['entities']
-    keys_to_extract = ['datatype', 'labels', 'descriptions', 'aliases']
+def copy_entities(session, source_api_url, target_api_url, target_csrf_token, id_list, language_list, entity_type='property', equiv_property=None):
+    entities_in = get_entities(source_api_url, id_list, language_list)
+    entities_in = entities_in['entities']
+    keys_to_extract = ['labels', 'descriptions', 'aliases']
+    if entity_type == 'property':
+        keys_to_extract.append('datatype')
+
     params = {
         "action": "wbeditentity",
-         "new": "property",
+         "new": entity_type,
          "bot": True,
          "token": target_csrf_token,
          "format": "json",
          "summary": "Bot edit!"
     }
-    property_mapping=[]
-    for pid in id_list:
-        data = {key: properties_in[pid][key] for key in keys_to_extract}
+    entity_mapping=[]
+    for entity_id in id_list:
+        data = {key: entities_in[entity_id][key] for key in keys_to_extract}
         if equiv_property:
             # Create claim linking this to equivalent property ID in Wikidata
             data['claims'] = [{
@@ -83,7 +86,7 @@ def copy_properties(session, source_api_url, target_api_url, target_csrf_token, 
                     'snaktype': 'value',
                     'property': equiv_property,
                     'datavalue': {
-                        'value': pid,
+                        'value': entity_id,
                         'type': 'string'
                     }
                 },
@@ -93,24 +96,26 @@ def copy_properties(session, source_api_url, target_api_url, target_csrf_token, 
         params['data'] = json.dumps(data)
         response = session.post(target_api_url, data=params)
         json_response = handle_response(response)
-        target_pid = json_response['entity']['id']
-        print("Created new property with ID {}".format(target_pid))
-        property_mapping.append({'source_id': pid, 'target_id': target_pid})
-    return property_mapping
+        target_id = json_response['entity']['id']
+        print("Created new entity with ID {}".format(target_id))
+        entity_mapping.append({'source_id': entity_id, 'target_id': target_id})
+    return entity_mapping
 
 def main():
     config = configparser.ConfigParser()
     config.read('.config')
     
-    parser = argparse.ArgumentParser(description='Load properties into Wikibase')
+    parser = argparse.ArgumentParser(description='Load entities into Wikibase')
     parser.add_argument('id_list_file', 
-                        help='Text file with list of properties to copy into Wikibase, one PID per line')
+                        help='Text file with list of entities to copy into Wikibase, one ID per line')
     parser.add_argument('language_list_file', 
                         help='Text file with list of language codes in which to copy terms, one code per line')
     parser.add_argument('output_directory', 
                         help='Directory in which to save ID mapping file')
+    parser.add_argument('type', choices=['property', 'item'],
+        help='Type of entities to load into Wikibase')
     parser.add_argument('--equiv_property', type=str, required=False,
-        help='PID (in target Wikibase) of property representing \'corresponding Wikidata property\'')
+        help='PID (in target Wikibase) of property representing \'corresponding Wikidata property\' or \'corresponding Wikidata item\'')
     
     args = parser.parse_args()
     with open(args.id_list_file) as file:
@@ -124,12 +129,13 @@ def main():
     s = requests.Session()
     wikibase_api_url, csrf_token, s = retrieve_credentials_and_get_token(s, config)
     
-    property_map = copy_properties(s, WIKIDATA_API_URL, wikibase_api_url, 
-                               csrf_token, id_list, language_list, args.equiv_property)
+    entity_map = copy_entities(s, WIKIDATA_API_URL, wikibase_api_url, 
+                               csrf_token, id_list, language_list, 
+                               entity_type=args.type, equiv_property=args.equiv_property)
     
-    output_file = os.path.join(args.output_directory, 'property_id_mapping.json')
+    output_file = os.path.join(args.output_directory, 'entity_id_mapping.json')
     with open(output_file, 'w') as fout:
-        json.dump(property_map, fout)
+        json.dump(entity_map, fout)
 
 if __name__ == '__main__':
     main()
